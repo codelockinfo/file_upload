0e0ecee755758ec40bd6edc6cfe9412ca6c4724c
codelockinfo <codelockinfo@gmail.com>
2024-01-29 15:10:15 +0530

    remove filess

--- user/dashboard_footer.php
+++
@@ -1,6 +0,0 @@
-<!--<center><div class="Polaris-FooterHelp__Content">
-                           <div class="Polaris-FooterHelp__Text">
-                                    ReWriter © 2021 - Developed by <a target="_blank" href="http://codelocksolutions.com/">Codelock</a>  </div> 
-        </div></center>
-
--->
